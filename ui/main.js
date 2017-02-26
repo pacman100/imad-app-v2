e22f958d7acae9fd580fde03e7286d39a26ed9e3
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-02-26 22:43:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,16 @@
 console.log('Loaded!');
+var element = document.getElementById("main_txt");
+element.innerHTML = 'Sourab Mangrulkar';
+var para = document.getElementById("para");
+para.innerHTML = 'Painting a masterpiece on the canvas of life. :)';
+var img = document.getElementById("madi");
+
+var marginLeft = 10;
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onClick = function() {
+    var inter = interval(moveRight , 50);
+}
 
