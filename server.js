512c508ec3cc01ae24bdbf0056f528c182c1a228
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-09 01:07:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/s.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 's.jpg'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
