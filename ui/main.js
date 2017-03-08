72aab29583b9bc1a5b5786d11190dc50f19a8984
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-09 01:00:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,18 +15,32 @@ img.onclick = function() {
 }*/
 
 
-//couonter logic
+//counter logic
 var button = document.getElementById('counter');
- var counter=0;
 button.onclick = function(){
-    //Make a request to counter endpoint
+    
+    
+    //create a request object
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
+            //take some action 
+            if(request.status === 200) {
+                var counter = request.responseText;
+                //Render the variable in correct span
+                var span= document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
     
-    //Render the variable in correct span
-   
-    var span= document.getElementById('count');
-    counter=counter+1;
-    span.innerHTML = counter.toString();
-}
+    //Make a request to counter endpoint
+    request.open('GET', 'http://pacman100.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+};
 
