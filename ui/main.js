17188c38613ac14b02fef19f34e0dd7dc6937b87
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-09 02:22:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,7 +76,7 @@ submit.onclick = function() {
     //Make a request to counter endpoint
     var nameInput= document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://pacman100.imad.hasura-app.io/submit-name?' + name, true);
+    request.open('GET', 'http://pacman100.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
   
 };
