3762cd7dcc240be78b343f7a9c15d98730314f77
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-09 02:07:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function() {
 }*/
 
 
-//counter logic
+//counter code
 var button = document.getElementById('counter');
 button.onclick = function(){
     
@@ -43,3 +43,40 @@ button.onclick = function(){
     request.send(null);
 };
 
+
+//comment code
+//submit name
+var submit = dcument.getElementById('submit');
+submit.onclick = function() {
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
+            //take some action 
+            if(request.status === 200) {
+                //render the names array list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++)
+                {
+                    list+= '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('nameslist');
+                ul.innerHTML = list;
+            }
+        }
+        //not done yet
+    };
+    
+    //Make a request to counter endpoint
+    var nameInput= document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://pacman100.imad.hasura-app.io/submit-name?' + name, true);
+    request.send(null);
+  
+};
