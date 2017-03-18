1d11414cd8cfc0435937d704363cb7fa91a5e1be
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-18 16:46:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,7 +50,7 @@ function loadLoginForm() {
         request.open('POST','/login-user',true);
         request.setRequestHeader('Content-Type','application/json');
         request.send(JSON.stringify({username: username , password: password}));
-        login_btn.value = "Loggin in...";
+        login_btn.value = "Logging in...";
     };
     
      //submit username & password to register
