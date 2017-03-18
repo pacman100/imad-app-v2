2a6b333eab74a87e27fb25d8696309d414193490
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-18 12:14:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -229,11 +229,11 @@ app.get('/articles/:articleName', function (req, res) {
 
 /*app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+});*/
 
 app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});*/
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
