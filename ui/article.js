18282de12ed1c5294bc5370fd0936a2c43c089b9
Sourab Mangrulkar <100rabmangrulkar@gmail.com>
2017-03-18 20:11:01 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -42,7 +42,7 @@ function loadCommentForm() {
         var comment = document.getElementById('comment_text');
         request.open('POST', '/submit-comment/'+currentArticleTitle, true);
         request.setRequestHeader('Content-Type','application/json');
-        concole.log(comment);
+        console.log(comment);
         request.send(JSON.stringify({comment: comment}));
         submit_btn.value = 'Submitting...';
     };
